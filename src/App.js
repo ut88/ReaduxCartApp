220c87f7e36347ed763c770fc616173bfe910f8c
Utkarsh Badoni <utkarshbadoni2000@gmail.com>
2023-05-25 17:02:45 +0530

    Fixing the Cart problems

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import { useSelector } from 'react-redux';
-import { useEffect } from 'react';
+import { Fragment } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect} from 'react';
+import Notification from './components/UI/Notification';
+
+import { sendCartData } from './components/store/cart-slice';
+
+
+let isInitial=true;
 
 function App() {
+  const dispatch=useDispatch();
  const showCart= useSelector(state=>state.ui.cartIsVisible)
  const cart= useSelector((state)=>state.cart)
+ const notification=useSelector(state=>state.ui.notification)
 
  useEffect(()=>{
-  fetch('https://otp-project-ceb7f-default-rtdb.firebaseio.com/cart.json',{
-    method:'PUT',
-    body:JSON.stringify(cart),
-  })
- },[cart])
+   dispatch(sendCartData(cart)) 
+  if(isInitial) {
+    isInitial=false;
+    return;
+  } 
+ },[cart,dispatch])
   return (
-    <Layout>
+    <Fragment>
+      {notification &&    <Notification status={notification.status}
+      title={notification.title}
+      message={notification.message} />}
+     <Layout>
      {showCart && <Cart />}
       <Products />
     </Layout>
+    </Fragment>
   );
 }
 
